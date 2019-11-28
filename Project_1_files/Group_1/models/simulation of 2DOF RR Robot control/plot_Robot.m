e4a01a1938cee0e9ab2187a3113f72a3bbd557dc
saar99 <53876747+saar99@users.noreply.github.com>
2019-11-28 13:45:43 +0200

    Motion plan

--- Project_1_files/Group_1/models/simulation of 2DOF RR Robot control/plot_Robot.m
+++ Project_1_files/Group_1/models/simulation of 2DOF RR Robot control/plot_Robot.m
@@ -1,7 +1,8 @@
-function [outputArg1,outputArg2] = plot_Robot(X_limit, Y_limit)
+function plot_Robot(t,l1, l2, theta1, theta2, X_limit, Y_limit, video)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+%create figure elements
 hFig = figure(1);
 hAxes = axes(hFig);
 hold on
@@ -14,14 +15,25 @@ hArm1 = line([nan,nan],[nan,nan],'LineWidth',3);
 set(hArm1, 'color', 'k')
 hArm2 = line([nan,nan],[nan,nan],'LineWidth',3);
 
-l1_line_end = [l1.*c1; l1.*s1];
-l2_line_end = [l2.*cos(theta1+theta2) ; l2.*sin(theta1+theta2)]+l1_line_end;
+c1 = cos(theta1);
+s1 = sin(theta1);
+c12 = cos(theta1+theta2);
+s12 = sin(theta1+theta2);
 
-Grip = zeros(2,length(X1(:,1)));  % initiate grip position log vector
+l1_line_end = [l1.*c1; l1.*s1];
+l2_line_end = [l2.*c12 ; l2.*s12]+l1_line_end;
+
+Grip = zeros(2,length(t));  % initiate grip position log vector
 hold on
 grid on
 
-for k=1:length(X1(:,1))
+if video == 1
+    v = VideoWriter('video1.avi');
+    open(v)
+end
+
+
+for k=1:length(t)
     
     Grip(1,k) = l2_line_end(1,k); % log grip position
     Grip(2,k) = l2_line_end(2,k);
@@ -30,13 +42,18 @@ for k=1:length(X1(:,1))
     set(hArm2,'XData',[l1_line_end(1,k), l2_line_end(1,k)],'YData',[l1_line_end(2,k) , l2_line_end(2,k)]);
     set(hMark,'XData',Grip(1,k),'YData',Grip(2,k));
     drawnow
-    xlim(hAxes,[-X_limit X_limit]);
-    pause(0.5)
-%     plot(Grip(1,:), Grip(2, :), '*g')
+    xlim(hAxes,[-3 3]);
+    
+    if video == 1
+        frame = getframe(gcf);
+        writeVideo(v,frame);
+    end
+    
+    plot(Grip(1,:), Grip(2, :), '*g')
    
 end
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+close(v)
+
 end
 
